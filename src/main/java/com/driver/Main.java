9c9c685fbe30ea39a87a571dc401214c26c617c9
Abhay Singh <abhaysingh07082000@gmail.com>
2022-12-27 09:04:26 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r1=new RWOnly("abhay");
+        System.out.println(r1.display());
+
+        r1.setName("chandu");
+
+        System.out.println( r1.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    RWOnly(String name)
+    {
+        this.name=name;
+    }
+    String display()
+    {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
